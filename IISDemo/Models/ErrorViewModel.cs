ffcab6bd212e71d75f1cc436424c39b02b435469
Shreyas Gavand <shreyasgavand9711@gmail.com>
2025-05-08 16:11:51 +0530

    Add project files.

---
+++ IISDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IISDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
